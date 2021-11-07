78cfa4b2888c890969490c3ac5ce2d6de36a3151
Coder-19 <arorajasman19@gmail.com>
2021-11-07 18:26:54 +0530

    removed unnecessary code

--- src/com/sortingPractice/bubbleSort/BubbleSortPractice.java
+++ src/com/sortingPractice/bubbleSort/BubbleSortPractice.java
@@ -7,9 +7,6 @@ public class BubbleSortPractice {
     // the code below is used to create a method to swap the elements in an array at index i and index j
     public void sort(int[] array,int i,int j){
 
-        // the below line of code is for debugging purpose
-        System.out.println("Inside sort method");
-
         // the code below is used to check if the index i is equal to the index j then return else do the swapping
         if(i == j){
             return;
--- src/com/sortingPractice/bubbleSort/Main.java
+++ src/com/sortingPractice/bubbleSort/Main.java
@@ -4,12 +4,6 @@ package com.sortingPractice.bubbleSort;
 public class Main {
 
     public static void main(String[] args) {
-        // the below line of code is for debugging purpose
-        System.out.println("Hello World");
-
-        // the code below is used to create an instance of the BubbleSortPractice class
-        BubbleSortPractice bubbleSortPractice = new BubbleSortPractice();
-        // the code below is used to call the sort method from the bubble sort practice class
 
     }
 }
