cd177f597389db08b86d2185aea945273b364006
Coder-19 <arorajasman19@gmail.com>
2021-11-08 17:58:23 +0530

    added the code to sort the elements of the array using bubble sort

--- src/com/sortingPractice/bubbleSort/BubbleSortPractice.java
+++ src/com/sortingPractice/bubbleSort/BubbleSortPractice.java
@@ -4,17 +4,44 @@ package com.sortingPractice.bubbleSort;
 // bubble sort
 public class BubbleSortPractice {
 
-    // the code below is used to create a method to swap the elements in an array at index i and index j
-    public void sort(int[] array,int i,int j){
+    // the code below is used to create a class / static function to sort the array using bubble sort and then print
+    // the array
+    public static void sort(int[] array){
+        // the code below is used to traverse the array from the last position to the first position
+        for(int lastSortedIndex = 0; lastSortedIndex > 0; lastSortedIndex--){
+            // the code below is used to traverse the from the first position to the element at
+            // the last sorted position
+            for(int index = 0; index < lastSortedIndex; index++){
+                // the code below is used to check that if the element at index is
+                // greater then the element at index + 1 position then swap the elements
+                if(array[index] > array[index + 1]){
+                    // the code below is to use the swap  method to swap the elements at index and index + 1 position
+                    swap(array, index, index + 1);
+                }
+            }
+        }
 
-        // the code below is used to check if the index i is equal to the index j then return else do the swapping
-        if(i == j){
-            return;
-        } else {
-            // the code below is used to create a variable to store the value at index i
-            int temp = array[i];
-            array[i] = array[j];
-            array[j] = temp;
+        System.out.println("The sorted elements of the array are: ");
+        // the code below is used to print the elements of the array after the elements are sorted
+        for(int element: array){
+            // the code below is used to print the elements of the array
+            System.out.println(element);
         }
     }
+
+    // the code below is used to create a method that takes the array and index i and index j and then swap the
+    // element at index i with the element at index j
+    public static void swap(int[] arr,int i,int j){
+        // the code below is used to check that if the value of index i is equal to the value of index j then
+        // return since both the index are same
+        if(i == j){
+            return;
+        }
+
+        // the code below is used to create a temporary variable to get the value of the element at index i
+        int temp = arr[i];
+        arr[j] = arr[i];
+        arr[i] = temp;
+    }
+
 }
--- src/com/sortingPractice/bubbleSort/Main.java
+++ src/com/sortingPractice/bubbleSort/Main.java
@@ -1,9 +1,37 @@
 package com.sortingPractice.bubbleSort;
 
+import java.util.Scanner;
+
 // the code below is used to create the main class for the project
 public class Main {
 
     public static void main(String[] args) {
+        // the code below is used to create an instance of the scanner class to get the input from the user
+        Scanner sc = new Scanner(System.in);
 
+        // the code below is used to create an array to get the elements from the user
+        int[] arr = new int[50];
+
+        // the code below is used to get the number of elements in the array from the user
+        System.out.println("Enter the number of elements in the array: ");
+        // the code below is used to get the number of elements in the array
+        int num = sc.nextInt();
+
+        //  the code below is used to get the array elements as input from the user
+        System.out.println("Enter the elements of the array: ");
+        for(int index = 0; index < num; index++){
+            // the code below is used to get the elements of the array from the user
+            arr[index] = sc.nextInt();
+        }
+
+        // the code below is used to print the elements of the array entered by the user
+        System.out.println("The elements in the array are: ");
+        for(int element: arr){
+            // the code below is used to print the elements of the array
+            System.out.println(element);
+        }
+
+        // the code below is used to call the sort class method using the BubbleSortPractice class
+        BubbleSortPractice.sort(arr);
     }
 }
